994489d230e87dcb6d8833e10777a3a6c5eaac78
luisgarciadiaz <zethes@gmail.com>
2020-01-15 17:19:24 -0600

    15-01-2020

---
+++ edit.php
@@ -0,0 +1,137 @@
+<?php
+include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Datos de empleados</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+	
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include("nav.php");?>
+	</nav>
+	<div class="container">
+		<div class="content">
+			<h2>Datos del empleados &raquo; Editar datos</h2>
+			<hr />
+			
+			<?php
+			// escaping, additionally removing everything that could be (html/javascript-) code
+			$codi = mysqli_real_escape_string($con,(strip_tags($_GET["codi"],ENT_QUOTES)));
+			$sql = mysqli_query($con, "SELECT * FROM empleados WHERE codigo='$codi'");
+			if(mysqli_num_rows($sql) == 0){
+				header("Location: index.php");
+			}else{
+				$row = mysqli_fetch_assoc($sql);
+			}
+			if(isset($_POST['save'])){
+				$codigo		     = mysqli_real_escape_string($con,(strip_tags($_POST["codigo"],ENT_QUOTES)));//Escanpando caracteres 
+				$nombres		     = mysqli_real_escape_string($con,(strip_tags($_POST["nombres"],ENT_QUOTES)));//Escanpando caracteres 
+				$lugar	 = mysqli_real_escape_string($con,(strip_tags($_POST["lugar"],ENT_QUOTES)));//Escanpando caracteres 
+				$fecha_alta	 = mysqli_real_escape_string($con,(strip_tags($_POST["fecha_alta"],ENT_QUOTES)));//Escanpando caracteres 
+				$direccion	     = mysqli_real_escape_string($con,(strip_tags($_POST["direccion"],ENT_QUOTES)));//Escanpando caracteres 
+				$telefono		 = mysqli_real_escape_string($con,(strip_tags($_POST["telefono"],ENT_QUOTES)));//Escanpando caracteres 				
+				$estado			 = mysqli_real_escape_string($con,(strip_tags($_POST["estado"],ENT_QUOTES)));//Escanpando caracteres  
+				$update = mysqli_query($con, "UPDATE empleados SET nombres='$nombres', lugar='$lugar', fecha_alta='$fecha_alta', direccion='$direccion', telefono='$telefono', estado='$estado' WHERE codigo='$codi'") or die(mysqli_error());
+				if($update){
+					header("Location: edit.php?codi=".$codi."&pesan=sukses");
+				}else{
+					echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error, no se pudo guardar los datos.</div>';
+				}
+			}
+			
+			if(isset($_GET['pesan']) == 'sukses'){
+				echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Los datos han sido guardados con éxito.</div>';
+			}
+			?>
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Código</label>
+					<div class="col-sm-2">
+						<input type="text" name="codigo" value="<?php echo $row ['codigo']; ?>" class="form-control" placeholder="codi" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nombres</label>
+					<div class="col-sm-4">
+						<input type="text" name="nombres" value="<?php echo $row ['nombres']; ?>" class="form-control" placeholder="Nombres" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Lugar Ingerencia</label>
+					<div class="col-sm-4">
+						<input type="text" name="lugar" value="<?php echo $row ['lugar']; ?>" class="form-control" placeholder="Lugar" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Fecha de alta</label>
+					<div class="col-sm-4">
+						<input type="text" name="fecha_alta" value="<?php echo $row ['fecha_alta']; ?>" class="input-group date form-control" date="" data-date-format="yyyy-mm-dd" placeholder="0000-00-00" required>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Dirección</label>
+					<div class="col-sm-3">
+						<textarea name="direccion" class="form-control" placeholder="Dirección"><?php echo $row ['direccion']; ?></textarea>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Teléfono</label>
+					<div class="col-sm-3">
+						<input type="text" name="telefono" value="<?php echo $row ['telefono']; ?>" class="form-control" placeholder="Teléfono" required>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Estado</label>
+					<div class="col-sm-3">
+						<select name="estado" class="form-control">
+							<option value="">- Selecciona estado -</option>
+                            <option value="1" <?php if ($row ['estado']==1){echo "selected";} ?>>Prof</option>
+							<option value="2" <?php if ($row ['estado']==2){echo "selected";} ?>>Local</option>
+							<option value="3" <?php if ($row ['estado']==3){echo "selected";} ?>>Nacional</option>
+						</select> 
+					</div>
+                   
+                </div>
+			
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="save" class="btn btn-sm btn-primary" value="Guardar datos">
+						<a href="index.php" class="btn btn-sm btn-danger">Cancelar</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+	$('.date').datepicker({
+		format: 'dd-mm-yyyy',
+	})
+	</script>
+</body>
+</html>
